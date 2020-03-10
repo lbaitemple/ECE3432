083b421e5f68060e339663c222f180e22ba56ea6
lbaitemple <lbai@temple.edu>
2020-03-10 18:00:37 -0400

    Update start.sh

--- VMWare/src/start.sh
+++ VMWare/src/start.sh
@@ -1,8 +1,10 @@
+##you can use img or csv format to save file
+export FORMAT="img"
+## add raspberry pi IP address, please start script in raspberry pi first
 export MASTERIP=10.110.42.68
 export ROS_MASTER_URI=http://$MASTERIP:11311
 export ROS_IP=`hostname  -I |sed 's/ //g'`
-##you can use img or csv format to save file
-export FORMAT="img"
+
 
 cd teleop_ws
 catkin_make
