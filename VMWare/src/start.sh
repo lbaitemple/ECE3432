e86ebe13c7a5112ab063971a20f501a28839e162
lbaitemple <lbai@temple.edu>
2020-03-10 17:58:50 -0400

    Add files via upload

--- VMWare/src/start.sh
+++ VMWare/src/start.sh
@@ -1,6 +1,8 @@
-export MASTERIP=10.110.41.228
+export MASTERIP=10.110.42.68
 export ROS_MASTER_URI=http://$MASTERIP:11311
 export ROS_IP=`hostname  -I |sed 's/ //g'`
+##you can use img or csv format to save file
+export FORMAT="img"
 
 cd teleop_ws
 catkin_make
